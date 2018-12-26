9613c4c70d7e459dff44ed4093d614ed6144963c
wa <15155313869@163.com>
2018-12-26 17:56:53 +0800

    authorizationServer create

--- authorization-server/src/main/java/com/wangan/qin/springsecurity/authorserver/AuthenticationServerApplication.java
+++ authorization-server/src/main/java/com/wangan/qin/springsecurity/authorserver/AuthenticationServerApplication.java
@@ -2,8 +2,10 @@ package com.wangan.qin.springsecurity.authorserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class AuthenticationServerApplication {
 
     public static void main(String[] args) {
--- authorization-server/src/main/java/com/wangan/qin/springsecurity/authorserver/config/AuthCodeServerConfig.java
+++ authorization-server/src/main/java/com/wangan/qin/springsecurity/authorserver/config/AuthCodeServerConfig.java
@@ -1,31 +1,48 @@
 package com.wangan.qin.springsecurity.authorserver.config;
 
+import com.wangan.qin.springsecurity.authorserver.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration
-@EnableAuthorizationServer
 public class AuthCodeServerConfig  extends AuthorizationServerConfigurerAdapter  {
+
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenStore tokenStore;
+    @Autowired
+    private CustomUserDetailService userDetailService;
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer authServer) throws Exception {
-        authServer.tokenKeyAccess("permitAll()")
-                .checkTokenAccess("isAuthenticated()");
+        authServer
+                .allowFormAuthenticationForClients()
+                .passwordEncoder(new BCryptPasswordEncoder())
+//                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("permitAll()");
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient("wangan").secret("111111").redirectUris("http://localhost:9001/authCodeCallback")
-                .authorizedGrantTypes("authorization_code")
-                .scopes("read_userinfo", "read_contacts");
+        clients.jdbc(dataSource);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        super.configure(endpoints);
+        endpoints.authenticationManager(authenticationManager)
+            .tokenStore(tokenStore).userDetailsService(userDetailService);
     }
 }
